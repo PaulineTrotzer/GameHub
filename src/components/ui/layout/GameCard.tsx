ebe730c30402258864afcb8a611073ed29d8279f
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2025-05-09 23:47:24 +0200

    Get faster loading images

--- src/components/ui/layout/GameCard.tsx
+++ src/components/ui/layout/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, Image, Heading, Box, Text, HStack } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "../PlatformIconList";
 import CriticScore from "../CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 interface Props {
   game: Game;
@@ -11,18 +12,15 @@ export default function GameCard({ game }: Props) {
   return (
     <Card.Root borderRadius="md" overflow="hidden" boxShadow="md">
       <Image
-        src={game.background_image}
+        src={getCroppedImageUrl(game.background_image)}
         alt={game.name}
         w="full"
-        h="300px"
-        objectFit="cover"
       />
 
-      <Card.Body p={4}>
-        <Heading as="h3" size="2xl" fontWeight="bold" textAlign="center" mb={2}>
-          {game.name}
-        </Heading>
-
+      <Heading as="h3" size="2xl" fontWeight="bold" textAlign="center" mb={2}>
+        {game.name}
+      </Heading>
+      <Card.Body gap="2" p={4}>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
